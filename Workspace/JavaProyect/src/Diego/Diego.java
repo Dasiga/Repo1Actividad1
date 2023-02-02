81ef81c97e168bf4efb80e65e28faa7a0f37239e
Diego de Silva Setién <dasiga97@gmail.com>
2023-02-02 21:11:11 +0100

    Mostrar nombre corrección

--- Workspace/JavaProyect/src/Diego/Diego.java
+++ Workspace/JavaProyect/src/Diego/Diego.java
@@ -2,8 +2,12 @@ package Diego;
 
 public class Diego {
 
-	public static void main(String[] args) {
-		System.out.println("Hola, soy Diego");
+	public static void main(String[] args) {		
+		System.out.println(mostrarNombre());
+	}
+	
+	public static String mostrarNombre() {
+		return "Hola, soy Diego";
 	}
 
 }
