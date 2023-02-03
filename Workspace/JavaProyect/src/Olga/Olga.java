b4afe8f55ccc4936d015e407f773b6e3da5a1065
Olgapichel <olgapd@hotmail.es>
2023-02-03 17:52:48 +0100

    Añado mis tres hobbies

--- Workspace/JavaProyect/src/Olga/Olga.java
+++ Workspace/JavaProyect/src/Olga/Olga.java
@@ -5,9 +5,13 @@ public class Olga {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(mostrarNombre());
+		System.out.println(misTresHobbies());
 	}
 	public static String mostrarNombre() {
 		return "Hola, soy Olga";
 	}
-
+	public static String misTresHobbies() {
+		return "mis tres hobbies son videojuegos, cocina y pirograbado";
+	}
+			
 }
