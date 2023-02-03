f930ac4b27ae93284f45924b51fffd78c29c4cef
unknown <aneurymorel@gmail.com>
2023-02-03 17:46:41 +0100

    Añadido mis tres hobbies

--- Workspace/JavaProyect/src/danilo/Danilo.java
+++ Workspace/JavaProyect/src/danilo/Danilo.java
@@ -5,11 +5,16 @@ public class Danilo {
 	public static void main(String[] args) {
 		
 		System.out.println(mostrarNombre());
+		System.out.println(tresHobbies());
 
 	}
 
 	public static String mostrarNombre() {
 		return "Hola, soy Danilo";
 	}
+	public static String tresHobbies() {
+		return "Videojuegos, bailar, pádel";
+	}
+	
 	
 }
