3128fd5bf7c35ab78acc78514a3e53636bcb963c
Diego de Silva Setién <dasiga97@gmail.com>
2023-02-03 13:30:27 +0100

    Metodo hobbies

--- Workspace/JavaProyect/src/Diego/Diego.java
+++ Workspace/JavaProyect/src/Diego/Diego.java
@@ -4,10 +4,15 @@ public class Diego {
 
 	public static void main(String[] args) {		
 		System.out.println(mostrarNombre());
+		System.out.println(mostrar3Hobbies());
 	}
 	
 	public static String mostrarNombre() {
 		return "Hola, soy Diego";
+	}
+	
+	public static String mostrar3Hobbies() {				
+		return "Hacer videojuegos, Jugar League of Legends y Ir al gimnasio";
 	}
 
 }
