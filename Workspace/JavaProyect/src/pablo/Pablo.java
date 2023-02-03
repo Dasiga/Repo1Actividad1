41e4fb049892ee3ef1fea17f110417345a7393e4
pablotecchia <acn.lde01@gmail.com>
2023-02-03 17:45:13 +0100

    Nuevo metodo hobbies

--- Workspace/JavaProyect/src/pablo/Pablo.java
+++ Workspace/JavaProyect/src/pablo/Pablo.java
@@ -4,6 +4,7 @@ public class Pablo {
 	public static void main(String[] args) {
 		
 		System.out.println(devolverNombre());
+		System.out.println(devolverHobbies());
 	
 		
 	}
@@ -13,7 +14,16 @@ public class Pablo {
 	public static String devolverNombre(){
 	
 	return "Hola, soy Pablo";
+	
+	}
+	
+	
+	public static String devolverHobbies() {
+		
+		return "Mis hobbies son nadar, electrónica y hacer proyectos DIY";
+		
+	}
 
 
 }
-}
+
