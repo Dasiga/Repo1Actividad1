e5e41b995b35ca6c12fb0af45b168ff2289a1aad
unknown <aneurymorel@gmail.com>
2023-02-03 01:23:36 +0100

    Mi primer commit

---
+++ Workspace/JavaProyect/src/danilo/Danilo.java
@@ -0,0 +1,15 @@
+package danilo;
+
+public class Danilo {
+
+	public static void main(String[] args) {
+		
+		System.out.println(mostrarNombre());
+
+	}
+
+	public static String mostrarNombre() {
+		return "Hola, soy Danilo";
+	}
+	
+}
